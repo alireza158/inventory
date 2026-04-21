718c7e45165e6843512ba2664d5f0a66c14052a2
alireza158 <alirezasajjadi158@gmail.com>
2026-04-21 12:06:49 +0330

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,14 +19,10 @@
     <!-- Bootstrap (اختیاری، فقط اگر نیاز داری) -->
     <script src="{{ asset('lib/bootstrap.bundle.min.js') }}"></script>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100..900&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}"/>
+     <link rel="stylesheet" href="{{ asset('lib/bootstrap.rtl.min.css') }}"/>
+ <link rel="stylesheet" href="{{ asset('lib/select2.min.css') }}"/>
+ <script src="{{ asset('lib/jquery-3.7.1.js') }}"></script>
 </head>
 <style>
     body, button, input, select, textarea {
